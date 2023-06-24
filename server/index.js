15e9ff0083e4f5576199382e5d3ce73bc6eaa67f
Saravanan43 <nithishviswanatha@gmail.com>
2023-06-24 21:49:11 +0530

    Add[Dummy commit]

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.status(200).send("Runnin");
+  return res.status(200).send("Running");
 });
 
 app.listen("3001", () => console.log("Server Running"));
